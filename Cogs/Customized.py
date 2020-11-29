7ccb12822af54f3f3715bf720cde020731e35082
ShibaInu0413 <neo123440@gmail.com>
2020-11-29 14:08:23 +0800

    upload

--- Cogs/Customized.py
+++ Cogs/Customized.py
@@ -3,13 +3,6 @@ from discord.utils import get as dget
 
 from Core import Cog_Extension
 
-class 女友芊芊的陪聊間(Cog_Extension):
-    @commands.Cog.listener()
-    async def on_member_join(self, member):
-        if member.guild.id == 613801330955386895:
-            role = dget(member.guild.roles, id=int("718652826661814283"))
-            await member.add_roles(role)
-
 class 粉紅色的羽毛堆(Cog_Extension):
     @commands.Cog.listener()
     async def on_member_join(self, member):
@@ -18,5 +11,4 @@ class 粉紅色的羽毛堆(Cog_Extension):
             await member.add_roles(role)
 
 def setup(client):
-    client.add_cog(女友芊芊的陪聊間(client))
     client.add_cog(粉紅色的羽毛堆(client))
